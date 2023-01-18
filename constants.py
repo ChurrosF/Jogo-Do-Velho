59404ad1b7fd1011ae8c6c5ddd9bd70063485b43
ChurrosF <106491489+ChurrosF@users.noreply.github.com>
2023-01-17 22:06:59 -0300

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,22 @@
+WIDTH = 600
+HEIGHT = 600
+
+ROWS = 3
+COLS = 3
+SQ_SIZE = HEIGHT // 3
+
+LINE_THICKNESS = 15
+
+# COLORS
+
+# BG_COLOR = (28, 170, 156)
+BG_COLOR = (249, 215, 28)
+
+# LINE_COLOR = (23, 145, 135)
+LINE_COLOR = (139, 128, 0)
+
+# IMAGES
+
+cerveja_path = 'TICTACTOE\\assets\\cerveja.png'
+cigarro_path = 'TICTACTOE\\assets\\cigarro.png'
+taco_path = 'TICTACTOE\\assets\\tacosinuca.png'
